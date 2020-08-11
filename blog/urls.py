b533a2b8975fe27685fb41f04868b68305ea56df
github.com/TIJANI0826 <ibrahimtijani08@gmail.com>
2020-08-11 01:40:25 +0100

    starter project for project portfolio and blog

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+urlpatterns = [
+    path("", views.blog_index, name="blog_index"),
+    path("<int:pk>/", views.blog_detail, name="blog_detail"),
+    path("<category>/", views.blog_category, name="blog_category"),
+]
