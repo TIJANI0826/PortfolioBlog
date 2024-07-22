bc74f88a10af203ec82671caa90b5d646459a3f7
TIJANI0826 <ibrahimtijani08@gmail.com>
2024-07-22 01:18:04 +0100

    added media url

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('projects.urls')),
     path("blog/",include('blog.urls')),
-]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
